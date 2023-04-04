ff9c35ac7ca03d57a9e02efd321f70ee5c691e66
21sudheer <sudheervarmap1999@gmail.com>
2023-04-04 22:17:51 +0530

    updated task on drop down

--- Mongodb1/app1/urls.py
+++ Mongodb1/app1/urls.py
@@ -2,14 +2,13 @@ from django import urls
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    path('', views.home, name='home'),
-    path('cvs/', views.cvs, name='cvs'),
-    path('get_all_data/', views.get_all_data, name='get_all_data'),
-    path('timespan/', views.timespan, name='timespan'),
-    path('get_sum/', views.get_sum, name='get_sum'),
-    path('get_avg/', views.get_avg, name='get_avg'),
-    path('get_bsavg/', views.get_bs_avg, name='get_bs_avg'),
-    path('get_sensors/', views.get_sensors, name='get_sensors'),
-
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("common/", views.common, name="common"),
+    path("get_all_data/", views.get_all_data, name="get_all_data"),
+    path("timespan/", views.timespan, name="timespan"),
+    path("get_sum/", views.get_sum, name="get_sum"),
+    path("get_avg/", views.get_avg, name="get_avg"),
+    path("get_bsavg/", views.get_bs_avg, name="get_bs_avg"),
+    path("get_sensors/", views.get_sensors, name="get_sensors"),
 ]
--- Mongodb1/app1/views.py
+++ Mongodb1/app1/views.py
@@ -5,6 +5,7 @@ from bson.objectid import ObjectId
 from pymongo import MongoClient
 from django.http import HttpResponse
 import csv
+import datetime
 
 connection = MongoClient()
 db = connection.querydata
@@ -16,7 +17,7 @@ def home(request):
     return render(request, template_name="home.html")
 
 
-def cvs(data, name):
+def common(data, name):
     print(data)
     response = HttpResponse(
         content_type="text/csv",
@@ -34,49 +35,96 @@ def cvs(data, name):
 # 1] From a given trip, give me all the data for "Sensor X" over time
 def get_all_data(request):
     print("hello")
+    sensor_data = db.vesseltripdata.aggregate(
+        [
+            {"$project": {"_id": 0}},
+        ]
+    )
+    sensor_name = None
+    from_time = None
+    to_time = None
 
-    if request.method == "GET":
-        print(request.GET.get("sensorName"))
-
-        trip_id = "63d4228e01afc1d6b6813e38"
+    if request.GET.get("sensorName"):
         sensor_name = request.GET.get("sensorName")
-        print(sensor_name)
-        from_time = datetime.now() - timedelta(days=90)
-        print(from_time)
-        to_time = datetime.now()
-        print(to_time)
+
         sensor_data = db.vesseltripdata.aggregate(
             [
                 {
                     "$match": {
-                        "dateTime": {"$gte": from_time, "$lte": to_time},
-                        "tripId": trip_id,
                         "sensorName": sensor_name,
                     }
                 },
                 {"$project": {"_id": 0}},
             ]
         )
-        print(sensor_data)
-        data = list(sensor_data)
-        # print(data)
-        # print(type(sensor_data))
-        if request.GET.get("export", None) == "True":
-            name = "cal"
-            res = cvs(data, name)
-            return res
-        return render(request, "index.html", {"data": data})
+    if request.GET.get("from"):
+        from_time = request.GET.get("from")
 
-    return HttpResponse("hi")
+    if request.GET.get("to"):
+        to_time = request.GET.get("to")
+
+    print(sensor_name, from_time, to_time)
+    if sensor_name and from_time and to_time:
+        format = "%Y-%m-%d"
+        from_time = datetime.datetime.strptime(from_time, format)
+        to_time = datetime.datetime.strptime(to_time, format)
+        sensor_data = db.vesseltripdata.aggregate(
+            [
+                {
+                    "$match": {
+                        "dateTime": {"$gte": from_time, "$lte": to_time},
+                        "sensorName": sensor_name,
+                    }
+                },
+                {"$project": {"_id": 0}},
+            ]
+        )
+
+    data = list(sensor_data)
+
+    if request.GET.get("export", None) == "True":
+        name = "get_all_data"
+        res = common(data, name)
+        return res
+    return render(request, "index.html", {"data": data})
 
 
 # 2] From a given timespan, give me all the data for "Sensor X" over time
 def timespan(request):
-    if request.method == "GET":
-        trip_id = "None"
+    sensor_data = db.vesseltripdata.aggregate(
+        [
+            {"$project": {"_id": 0}},
+        ]
+    )
+    sensor_name = None
+    from_time = None
+    to_time = None
+
+    if request.GET.get("sensorName"):
         sensor_name = request.GET.get("sensorName")
-        from_time = datetime.now() - timedelta(days=90)
-        to_time = datetime.now()
+
+        sensor_data = db.vesseltripdata.aggregate(
+            [
+                {
+                    "$match": {
+                        "sensorName": sensor_name,
+                    }
+                },
+                {"$project": {"_id": 0}},
+            ]
+        )
+    if request.GET.get("from"):
+        from_time = request.GET.get("from")
+
+    if request.GET.get("to"):
+        to_time = request.GET.get("to")
+
+    print(sensor_name, from_time, to_time)
+
+    if sensor_name and from_time and to_time:
+        format = "%Y-%m-%d"
+        from_time = datetime.datetime.strptime(from_time, format)
+        to_time = datetime.datetime.strptime(to_time, format)
         sensor_data = db.vesseltripdata.aggregate(
             [
                 {
@@ -88,80 +136,73 @@ def timespan(request):
                 {"$project": {"_id": 0}},
             ]
         )
-        # print(sensor_data)
-        data = list(sensor_data)
-        # print(data)
-        print(type(data))
-        if request.GET.get("done", None) == "True":
-            name = "info"
-            res = cvs(data, name)
-            return res
 
-        return render(request, "info.html", {"data": data})
-    return HttpResponse("working")
+    data = list(sensor_data)
+    if request.GET.get("done", None) == "True":
+        name = "timespan"
+        res = common(data, name)
+        return res
+    return render(request, "info.html", {"data": data})
 
 
 # 3] From a given trip, give the sum of "Sensor X" throughout the entire trip
 def get_sum(request):
-    if request.method == "GET":
-        trip_id = "63d4228e01afc1d6b6813e38"
-        sensor_name = "GPS"
-        sensor_sum = db.vesseltripdata.aggregate(
-            [
-                {"$match": {"tripId": trip_id, "sensorName": sensor_name}},
-                {"$addFields": {"arraySize": {"$size": "$dataPoints"}}},
-                {
-                    "$group": {
-                        "_id": "$sensorName",
-                        "index0_sum": {"$sum": {"$arrayElemAt": ["$dataPoints", 0]}},
-                        "index1_sum": {"$sum": {"$arrayElemAt": ["$dataPoints", 1]}},
-                        "index2_sum": {"$sum": {"$arrayElemAt": ["$dataPoints", 2]}},
-                    }
-                },
-            ]
-        )
-        print(sensor_sum)
-        print(type(sensor_sum))
-        data = list(sensor_sum)
-        print(data)
-        if request.GET.get("done", None) == "False":
-            name = "get_Sum"
-            result = cvs(data, name)
-            return result
-        return render(request, "get_sum.html", {"data": data})
-    return HttpResponse("END")
+
+    trip_id = "63d4228e01afc1d6b6813e38"
+    sensor_name = "GPS"
+    sensor_sum = db.vesseltripdata.aggregate(
+        [
+            {"$match": {"tripId": trip_id, "sensorName": sensor_name}},
+            {"$addFields": {"arraySize": {"$size": "$dataPoints"}}},
+            {
+                "$group": {
+                    "_id": "$sensorName",
+                    "index0_sum": {"$sum": {"$arrayElemAt": ["$dataPoints", 0]}},
+                    "index1_sum": {"$sum": {"$arrayElemAt": ["$dataPoints", 1]}},
+                    "index2_sum": {"$sum": {"$arrayElemAt": ["$dataPoints", 2]}},
+                }
+            },
+        ]
+    )
+    print(sensor_sum)
+    print(type(sensor_sum))
+    data = list(sensor_sum)
+    print(data)
+    if request.GET.get("done", None) == "False":
+        name = "get_sum"
+        result = common(data, name)
+        return result
+    return render(request, "get_sum.html", {"data": data})
 
 
 # 4] From a given trip, give the average of "Sensor X" throughout the entire trip</h2>
 def get_avg(request):
-    if request.method == "GET":
-        trip_id = "63d4228e01afc1d6b6813e38"
-        sensor_name = "GPS"
-        sensor_avg = db.vesseltripdata.aggregate(
-            [
-                {"$match": {"tripId": trip_id, "sensorName": sensor_name}},
-                {"$addFields": {"arraySize": {"$size": "$dataPoints"}}},
-                {
-                    "$group": {
-                        "_id": "$sensorName",
-                        "index0_avg": {"$avg": {"$arrayElemAt": ["$dataPoints", 0]}},
-                        "index1_avg": {"$avg": {"$arrayElemAt": ["$dataPoints", 1]}},
-                        "index2_avg": {"$avg": {"$arrayElemAt": ["$dataPoints", 2]}},
-                    }
-                },
-            ]
-        )
-        print(sensor_avg)
-        print(type(sensor_avg))
-        data = list(sensor_avg)
-        print(data)
-        if request.GET.get("done", None) == "True":
-            name = "get_avg"
-            result = cvs(data, name)
-            return result
 
-        return render(request, "get_avg.html", {"data": data})
-    return HttpResponse("hi")
+    trip_id = "63d4228e01afc1d6b6813e38"
+    sensor_name = "GPS"
+    sensor_avg = db.vesseltripdata.aggregate(
+        [
+            {"$match": {"tripId": trip_id, "sensorName": sensor_name}},
+            {"$addFields": {"arraySize": {"$size": "$dataPoints"}}},
+            {
+                "$group": {
+                    "_id": "$sensorName",
+                    "index0_avg": {"$avg": {"$arrayElemAt": ["$dataPoints", 0]}},
+                    "index1_avg": {"$avg": {"$arrayElemAt": ["$dataPoints", 1]}},
+                    "index2_avg": {"$avg": {"$arrayElemAt": ["$dataPoints", 2]}},
+                }
+            },
+        ]
+    )
+    print(sensor_avg)
+    print(type(sensor_avg))
+    data = list(sensor_avg)
+    print(data)
+    if request.GET.get("done", None) == "True":
+        name = "get_avg"
+        result = common(data, name)
+        return result
+    return render(request, "get_avg.html", {"data": data})
 
 
 # 5] From a given boat, give the average of “Sensor X” for each day between these dates
@@ -198,7 +239,7 @@ def get_bs_avg(request):
     print(data)
     if request.GET.get("export", None) == "True":
         name = "get_bs_avg"
-        res = cvs(data, name)
+        res = common(data, name)
         return res
     return render(request, "get_bs_avg.html", {"data": data})
 
@@ -218,7 +259,7 @@ def get_sensors(request):
     print(data)
     if request.GET.get("export", None) == "True":
         name = "get_sensors"
-        result = cvs(data, name)
+        result = common(data, name)
         return result
     # return HttpResponse("hi")
     return render(request, "get_sensors.html", {"data": data})
